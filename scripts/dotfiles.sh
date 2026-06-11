0271c087251d64a381a5e45b8c80957792a1d4e8
Damet24 <damet.2401@gmail.com>
2026-06-10 20:28:19 -0500

    update config

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-cp -r ~/.config/hypr/ ~/dotfiles/config/
-cp -r ~/.config/rofi/ ~/dotfiles/config/
-cp -r ~/.config/kitty/ ~/dotfiles/config/
-cp ~/.config/starship.toml ~/dotfiles/config/
-cp ~/.zshrc ~/dotfiles/config/
+
+DOTFILES=~/dotfiles/config/
+
+cp -r ~/.config/hypr/ $DOTFILES
+cp -r ~/.config/rofi/ $DOTFILES
+cp -r ~/.config/kitty/ $DOTFILES
+cp ~/.config/starship.toml $DOTFILES
+cp ~/.zshrc $DOTFILES
+cp ~/.config/ghostty/ $DOTFILES 
 
 cd ~/dotfiles/
 git add --all
