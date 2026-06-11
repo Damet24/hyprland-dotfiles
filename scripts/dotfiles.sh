55979bc0559fcbc3be4e68130eda2779a2ec941f
Damet24 <damet.2401@gmail.com>
2026-06-10 20:29:45 -0500

    ghostty config

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -5,7 +5,7 @@ cp -r ~/.config/rofi/ ~/dotfiles/config/
 cp -r ~/.config/kitty/ ~/dotfiles/config/
 cp ~/.config/starship.toml ~/dotfiles/config/
 cp ~/.zshrc ~/dotfiles/config/
-cp ~/.config/ghostty/ ~/dotfiles/config/ 
+cp -r ~/.config/ghostty/ ~/dotfiles/config/ 
 
 cd ~/dotfiles/
 git add --all
