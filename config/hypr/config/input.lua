2da915a9cf1fd2bb4be34bee2679965889716d43
Damet24 <damet.2401@gmail.com>
2026-06-12 08:02:59 -0500

    config updated

--- config/hypr/config/input.lua
+++ config/hypr/config/input.lua
@@ -1,9 +1,9 @@
 return {
 	input = {
-		kb_layout = "us, us",
-		kb_variant = ",colemak",
+		kb_layout = "us",
+		kb_variant = "colemak",
 		kb_model = "",
-		kb_options = "grp:win_space_toggle",
+		kb_options = "",
 		kb_rules = "",
 		follow_mouse = 1,
 		sensitivity = 0,
